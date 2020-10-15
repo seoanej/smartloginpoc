6d55d32c2393e995c9301ced87a5d2818b77c971
Jaime Seoane <jaime.b.seoane@gmail.com>
2020-10-15 22:14:08 +0200

    Added instrumentation as to collect metrics for prometheus

--- smarlogin/app/main.py
+++ smarlogin/app/main.py
@@ -1,14 +1,47 @@
 ''' SmartLogin Kubernetes PoC  webserver'''
+import json
 import socket
+import time
+
 from aiohttp import web
+from prometheus_client import Summary, Counter, Histogram, Gauge
+from prometheus_client.core import CollectorRegistry
+import prometheus_client
+
+
+_INF = float("inf")
+
+graphs = {}
+graphs['c'] = Counter('slpocapp_request_operations_total',
+                      'The total number of processed requests')
+graphs['h'] = Histogram('slpocapp_request_duration_seconds',
+                        'Histogram for the duration in seconds.', buckets=(1, 2, 5, 6, 10, _INF))
 
 
 async def handle_view(request):
+    start = time.time()
+    graphs['c'].inc()
     output = socket.getfqdn()
+#     time.sleep(0.600)
+    end = time.time()
+    graphs['h'].observe(end - start)
+    output = str(output) + ' ' + "processing time: " + str(end)
     return web.json_response({"Respuesta de:": output})
+
+
+async def handle_view_metrics(request):
+    res = []
+    for k, v in graphs.items():
+        res.append(prometheus_client.generate_latest(v))
+
+    output = ''
+    output = output.join(list(map(lambda s: s.decode('UTF-8'), res)))
+
+    return web.Response(body=output)
 
 app = web.Application()
 app.add_routes([web.get('/', handle_view)])
+app.add_routes([web.get('/metrics', handle_view_metrics)])
 
 if __name__ == '__main__':
     web.run_app(app, host='0.0.0.0', port=8000)
